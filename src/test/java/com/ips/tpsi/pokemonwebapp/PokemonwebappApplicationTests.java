35518e754a58880d3e624a7abe7465e7874678a4
joaocotrimcosta <115737003+joaocotrimcosta@users.noreply.github.com>
2023-11-18 13:13:38 +0000

    Comit inicial

---
+++ src/test/java/com/ips/tpsi/pokemonwebapp/PokemonwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ips.tpsi.pokemonwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
